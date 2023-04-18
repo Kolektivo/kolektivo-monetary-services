c34cfccb745666daed00b4a7c5b37e2eb7adbc29
Doug Kent <github@douglaskent.com>
2023-04-18 10:13:46 -0600

    loading ABIs is a go

--- src/index.ts
+++ src/index.ts
@@ -2,49 +2,69 @@
 import { Relayer } from "defender-relay-client";
 import { RelayerModel, RelayerParams } from "defender-relay-client/lib/relayer";
 
-// const IS_TESTING = require.main === module;
-// let abis = "";
-// const fetchABIs = async () => {
-//   if (abis === "") {
-//     const from = IS_TESTING
-//       ? "https://raw.githubusercontent.com/Kolektivo/kolektivo-monetary-contracts/development/exports/celo-test.json?token=GHSAT0AAAAAACA465CW4KWNT34WZRO7VHM2ZB54IEQ"
-//       : "https://raw.githubusercontent.com/Kolektivo/kolektivo-monetary-contracts/development/exports/celo.json?token=GHSAT0AAAAAACA465CXSXIZ7CAD3WACJOAKZB55EVQ";
-//     const response = await fetch(from);
-//     abis = await response.json();
-//   }
-//   return abis;
-// };
-// const async getContractAbi = (
-//   contractType: TContractType,
-//   name: Extract<keyof ContractGroupsJsons[TContractType]['main']['contracts'], string>
-// ): Promise<string> => {
-//   const contractData = (
-//     IS_TESTING ? ((await fetchABIS(`../../contracts/${contractType}/celo-test.json`)) as unknown) : ((await import(`../../contracts/${contractType}/celo.json`)) as unknown)
-//   );
-//   const contracts = contractData.contracts;
-//   const contract = contracts[name as keyof typeof contracts] as unknown as { abi: string | ContractInterface; address: string };
-//   let abi = contract.abi;
-//   if (typeof abi === 'string') {
-//     const key = abi as keyof ContractGroupsSharedJson;
-//     abi = (await this.getSharedAbi(contractType, key)) as ContractInterface;
-//   }
-//   overrideAddress = overrideAddress ?? contract.address;
-//   if (!overrideAddress) {
-//     throw new Error(`ContractService: requested contract has no address: ${name}`);
-//   }
-//   return abi;
-// }
-// const async getSharedAbi = (contractType: TContractType, key: keyof ContractGroupsJsons[TContractType]['shared']) => {
-//   const abis = await fetchABIs();
-//   // console.log(abis)
-//   const contractData = (await import(`../../contracts/${contractType}/sharedAbis.json`)) as ContractGroupsJsons[TContractType]['shared'];
-//   return contractData[key];
-// }
+interface IContractInfo {
+  address: string;
+  abi: Array<any>;
+}
+
+interface IContractInfosJson {
+  name: string;
+  chainId: number;
+  contracts: {
+    [name: string]: IContractInfo;
+  }
+}
+
+interface ISharedContractInfos {
+  [name: string]: Array<any>;
+}
+
+const IS_TESTING = require.main === module;
+let abis: IContractInfosJson;
+let sharedAbis: ISharedContractInfos;
+
+const fetchAbis = (): void => {
+  if (!abis) {
+    console.log("fetching abis");
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    abis = require(`./abis/${IS_TESTING ? "celo-test.json" : "celo.json"}`);
+    if (!abis) {
+      throw new Error("abis not found");
+    }
+
+    sharedAbis = require(`./abis/sharedAbis.json`);
+  }
+};
+
+const getContractAbi = (contractName: string): Array<any> => {
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+  let abi = abis.contracts[contractName]?.abi;
+  if (typeof abi === "string") {
+    // is name of shared abi, such as ERC20
+    abi = sharedAbis[abi];
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+  } else if (!abi) {
+    abi = sharedAbis[contractName];
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+  if (!abi) {
+    throw new Error(`abi not found for ${contractName}`);
+  }
+
+  return abi;
+};
 
 // Entrypoint for the Autotask
 export async function handler(credentials: RelayerParams /*, context: { notificationClient?: { send: (...) => void } }*/): Promise<string> {
+  fetchAbis();
   const relayer = new Relayer(credentials);
   const info: RelayerModel = await relayer.getRelayer();
+
+  // const oracleAbi = getContractAbi("Oracle");
+
+  // console.log(`oracleAbi[0].inputs: ${JSON.stringify(oracleAbi[0].inputs)}`);
+
   console.log(`Relayer address is ${info.address}`);
 
   // const { notificationClient } = context;
@@ -84,7 +104,8 @@ if (require.main === module) {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   require("dotenv").config();
   const { API_KEY: apiKey, API_SECRET: apiSecret } = process.env as EnvInfo;
-  // console.log("API_KEY", apiKey, "API_SECRET", apiSecret);
+
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
 
   handler({ apiKey, apiSecret } /*, { notificationClient: undefined }*/)
     .then(() => process.exit(0))
@@ -93,3 +114,4 @@ if (require.main === module) {
       process.exit(1);
     });
 }
+
