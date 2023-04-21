c95e6802180e032a92d6952299f4f5584a432e5b
Doug Kent <github@douglaskent.com>
2023-04-21 07:37:38 -0600

    catch errors in all services

--- src/kcur-floor-and-ceiling-service.ts
+++ src/kcur-floor-and-ceiling-service.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 
 import { getContract } from "./contracts-service";
+import { serviceThrewException } from "./errors-service";
 
 import { DefenderRelaySigner } from "defender-relay-client/lib/ethers/signer";
 import { Contract } from "ethers/lib/ethers";
@@ -9,26 +10,30 @@ import { formatEther } from "ethers/lib/utils";
 export const executeFloorAndCeilingService = async (kCurPrice: number, signer: DefenderRelaySigner): Promise<void> => {
   console.log("executing  the FloorAndCeilingService");
 
-  const kGuilderPrice = 1.79;
+  try {
+    const kGuilderPrice = 1.79;
 
-  const reserveContract = getContract("Reserve", signer);
-  const kCurContract = getContract("kCur", signer);
+    const reserveContract = getContract("Reserve", signer);
+    const kCurContract = getContract("kCur", signer);
 
-  //price floor is defined in the BL as Reserve Value / kCUR Supply
-  const reserveValue = Number.parseFloat(formatEther((await reserveContract.reserveStatus())[0]));
-  const kCurSupplyValue = Number.parseFloat(formatEther(await kCurContract.totalSupply())) * kCurPrice;
+    //price floor is defined in the BL as Reserve Value / kCUR Supply
+    const reserveValue = Number.parseFloat(formatEther((await reserveContract.reserveStatus())[0]));
+    const kCurSupplyValue = Number.parseFloat(formatEther(await kCurContract.totalSupply())) * kCurPrice;
 
-  if (!kCurSupplyValue) {
-    throw new Error("kCur totalSupply is zero");
+    if (!kCurSupplyValue) {
+      throw new Error("kCur totalSupply is zero");
+    }
+    const floor = reserveValue / kCurSupplyValue;
+    console.log(`reserve floor: ${floor}`);
+
+    //price ceiling is defined in the BL as Price Floor * Ceiling Multiplier
+    //TODO get ceiling multiplier from the proxy contract when we get it
+    const ceiling = floor * 1.9;
+    console.log(`reserve ceiling: ${ceiling}`);
+
+    const proxyContract = {} as Contract; // getContract("proxy", signer);
+  } catch (ex) {
+    serviceThrewException("Floor and Ceiling Service", ex);
   }
-  const floor = reserveValue / kCurSupplyValue;
-  console.log(`reserve floor: ${floor}`);
-
-  //price ceiling is defined in the BL as Price Floor * Ceiling Multiplier
-  //TODO get ceiling multiplier from the proxy contract when we get it
-  const ceiling = floor * 1.9;
-  console.log(`reserve ceiling: ${ceiling}`);
-
-  const proxyContract = {} as Contract; // getContract("proxy", signer);
   return await Promise.resolve();
 };
--- src/mento-arbitrage-service.ts
+++ src/mento-arbitrage-service.ts
@@ -1,14 +1,20 @@
 /* eslint-disable no-console */
 
 import { getContract } from "./contracts-service";
+import { serviceThrewException } from "./errors-service";
 
 import { DefenderRelaySigner } from "defender-relay-client/lib/ethers/signer";
 
 export const executeMentoService = async (kCurPrice: number, signer: DefenderRelaySigner): Promise<void> => {
   console.log("executing  the MentoService");
 
-  const kGuilderPrice = 1.79;
-  const kCurKGuilderRatio = 0;
-  const kGuilderPool = getContract("kGuilder Pool", signer); // getContract("kGuilderPool", signer);
+  try {
+    const kGuilderPrice = 1.79;
+    const kCurKGuilderRatio = 0;
+    const kGuilderPool = getContract("kGuilder Pool", signer); // getContract("kGuilderPool", signer);
+    throw new Error("Something went wrong");
+  } catch (ex) {
+    serviceThrewException("Mento Arbitrage Service", ex);
+  }
   return await Promise.resolve();
 };
