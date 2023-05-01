dcb72826b887ed2a8ac738afecf1bfc41ab3f161
Doug Kent <github@douglaskent.com>
2023-05-01 13:21:52 -0600

    make allowance setting work right

--- src/services/mento-arbitrage-service.ts
+++ src/services/mento-arbitrage-service.ts
@@ -1,9 +1,10 @@
-import { parseEther } from "ethers/lib/utils";
+import { formatEther, parseEther } from "ethers/lib/utils";
 import { KGUILDER_USDPRICE } from "../globals";
 import { getContract } from "../helpers/contracts-helper";
 import { logMessage, serviceThrewException } from "../helpers/errors-helper";
 
 import { DefenderRelaySigner } from "defender-relay-client/lib/ethers/signer";
+import { getContractAddress } from "../helpers/abi-helper";
 
 const serviceName = "Mento Service";
 const MENTO_BADGE_ID = 42042;
@@ -17,6 +18,9 @@ const sendBacBuy = async (
   relayerAddress: string,
   buyAmount: number,
   maxSellAmount: number,
+  /**
+   * gold is kCUR, !gold is kG
+   */
   buyGold: boolean,
 ): Promise<{ hash: string }> => {
   const bac = getContract("BAC", signer);
@@ -51,19 +55,36 @@ export const executeMentoService = async (
     // const kGuilderPrice = KGUILDER_USDPRICE;
     // const kCurKGuilderRatio = 0;
     // const kGuilderPool = getContract("kGuilder Pool", signer); // getContract("kGuilderPool", signer);
-    // const kCurContract = getContract("CuracaoReserveToken", signer);
+    const kCurContract = getContract("CuracaoReserveToken", signer);
+    // const kGContract = getContract("KolektivoGuilder", signer);
 
-    // how much to buy
+    /**
+     * fake buying kG
+     */
+    // how much kG to buy
     const buyAmount = 0.00001;
-    // max to get in return
+    // max to kCUR to get in return
     const maxSellAmount = 0.00001;
-    // kGTokenContract.approve(mentoOracleContract, kCurContractAmountToBeTransferred);
-    // kCurContract.approve(mentoOracleContract, kCurContractAmountToBeTransferred);
+    /**
+     * aprove withdrawal from the spend account
+     */
+    const mentoExchangeAddress = getContractAddress("MentoExchange");
+    /**
+     * tell kCUR token to allow the MentoExchange (spender) to spend kCUR on behalf of the Relayer (owner)
+     */
+    const currentAllowance = Number.parseFloat(
+      formatEther(await kCurContract.allowance(relayerAddress, mentoExchangeAddress)),
+    );
+    if (currentAllowance < maxSellAmount) {
+      const tx = await kCurContract.connect(signer).approve(mentoExchangeAddress, parseEther(maxSellAmount.toString()));
+      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+      logMessage(serviceName, `Approved max purchase of ${maxSellAmount}: ${tx.hash}`);
+    }
 
     const tx = await sendBacBuy(signer, relayerAddress, buyAmount, maxSellAmount, false);
 
     // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-    logMessage(serviceName, `Executed Mento Arbitrage, tx hash: ${tx.hash}`);
+    logMessage(serviceName, `Attempted to buy ${buyAmount} kG with max kCUR ${maxSellAmount}, tx hash: ${tx.hash}`);
 
   } catch (ex) {
     serviceThrewException(serviceName, ex);
