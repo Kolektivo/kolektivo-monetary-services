4a432419192ab4de578d68658c1c83f522db5fcb
Doug Kent <github@douglaskent.com>
2023-05-01 13:52:34 -0600

    expand capability of mento arbitrage

--- src/services/mento-arbitrage-service.ts
+++ src/services/mento-arbitrage-service.ts
@@ -13,26 +13,38 @@ const MENTO_BADGE_ID = 42042;
  * how to use the badger to gain access to the mentoservice:
  * https://github.com/Kolektivo/kolektivo-governance-contracts/pull/45
  */
-const sendBacBuy = async (
+const sendBuyOrSell = async (
   signer: DefenderRelaySigner,
   relayerAddress: string,
-  buyAmount: number,
-  maxSellAmount: number,
   /**
-   * gold is kCUR, !gold is kG
+   * amount paying if selling, or amount receiving if buying
    */
-  buyGold: boolean,
+  amount: number,
+  /**
+   * minimum amount receiving if selling, or maximum amount paying if buying
+   */
+  amountLimit: number,
+  /**
+   * In MentoExchange, gold refers to kCUR, !gold refers to kG
+   * When buying: gold is to buy kCUR, !gold is to buy kG
+   * When selling: gold is to sell kCUR, !gold is to sell kG
+   */
+  gold: boolean,
+  /**
+   * false to sell
+   */
+  buy: boolean,
 ): Promise<{ hash: string }> => {
   const bac = getContract("BAC", signer);
 
   const mentoExchange = getContract("MentoExchange", signer);
 
   // get function params for execTransactionFromModule from TransactionLike object
-  const txLike = await mentoExchange.populateTransaction.buy(
+  const txLike = await mentoExchange.populateTransaction[buy ? "buy" : "sell"](
     relayerAddress,
-    parseEther(buyAmount.toString()),
-    parseEther(maxSellAmount.toString()),
-    buyGold,
+    parseEther(amount.toString()),
+    parseEther(amountLimit.toString()),
+    gold,
   );
   const { to, value, data } = txLike;
 
@@ -61,10 +73,10 @@ export const executeMentoService = async (
     /**
      * fake buying kG
      */
-    // how much kG to buy
-    const buyAmount = 0.00001;
-    // max to kCUR to get in return
-    const maxSellAmount = 0.00001;
+    // how much kG to buy with kCUR
+    const receiveAmount = 0.00001;
+    // max kG to receive in return
+    const maxPayAmount = 0.00001;
     /**
      * aprove withdrawal from the spend account
      */
@@ -75,17 +87,16 @@ export const executeMentoService = async (
     const currentAllowance = Number.parseFloat(
       formatEther(await kCurContract.allowance(relayerAddress, mentoExchangeAddress)),
     );
-    if (currentAllowance < maxSellAmount) {
-      const tx = await kCurContract.connect(signer).approve(mentoExchangeAddress, parseEther(maxSellAmount.toString()));
+    if (currentAllowance < maxPayAmount) {
+      const tx = await kCurContract.connect(signer).approve(mentoExchangeAddress, parseEther(maxPayAmount.toString()));
       // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-      logMessage(serviceName, `Approved max purchase of ${maxSellAmount}: ${tx.hash}`);
+      logMessage(serviceName, `Approved max purchase of ${maxPayAmount}: ${tx.hash}`);
     }
 
-    const tx = await sendBacBuy(signer, relayerAddress, buyAmount, maxSellAmount, false);
+    const tx = await sendBuyOrSell(signer, relayerAddress, receiveAmount, maxPayAmount, false, true);
 
     // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-    logMessage(serviceName, `Attempted to buy ${buyAmount} kG with max kCUR ${maxSellAmount}, tx hash: ${tx.hash}`);
-
+    logMessage(serviceName, `Attempted to buy ${receiveAmount} kG with max kCUR ${maxPayAmount}, tx hash: ${tx.hash}`);
   } catch (ex) {
     serviceThrewException(serviceName, ex);
   }
