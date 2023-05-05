7da3d76271d470a4c854cb4af4dc24e8deb69d20
Doug Kent <github@douglaskent.com>
2023-05-04 19:52:11 -0600

    much better mento computations

--- src/services/mento-arbitrage-service.ts
+++ src/services/mento-arbitrage-service.ts
@@ -1,10 +1,10 @@
 import { ITransaction, KGUILDER_USDPRICE } from "../globals";
-import { fromWei, getContract } from "../helpers/contracts-helper";
+import { fromWei, fromWeiToNumber, getContract, toWei } from "../helpers/contracts-helper";
 import { logMessage, serviceThrewException } from "../helpers/errors-helper";
 import { IErc20Token } from "../helpers/tokens-helper";
 
 import { DefenderRelaySigner } from "defender-relay-client/lib/ethers/signer";
-import { BigNumber, FixedNumber } from "ethers";
+import { FixedNumber } from "ethers";
 
 const serviceName = "Mento Service";
 
@@ -21,67 +21,80 @@ export const executeMentoService = async (
     const mentoReserveContract = getContract("MentoReserve", signer);
     logMessage(serviceName, `MentoReserve address is: ${mentoReserveContract.address}`);
 
-    const kCurTotalValue = BigNumber.from(
-      FixedNumber.fromValue(await kCurContract.balanceOf(mentoReserveContract.address), 0, "fixed32x18")
+    const kCurTotalSupply = await kCurContract.balanceOf(mentoReserveContract.address);
+
+    const kCurTotalValue = fromWeiToNumber(
+      FixedNumber.fromValue(kCurTotalSupply, 0, "fixed32x18")
         .mulUnsafe(FixedNumber.fromString(kCurPrice.toString(), "fixed32x18"))
-        .addUnsafe(FixedNumber.fromString("1", "fixed32x18")) // round up one wei
+        // .addUnsafe(FixedNumber.fromString("1", "fixed32x18")) // round up one wei
         .round(0)
         .toFormat("fixed32x0")
         .toString(),
+      18,
     );
+
+    const kGTotalSupply = await kGContract.totalSupply();
 
     /**
      * using the fixed price of kG
      */
-    const kGTotalValue = BigNumber.from(
-      FixedNumber.fromValue(await kGContract.totalSupply(), 0, "fixed32x18")
+    const kGTotalValue = fromWeiToNumber(
+      FixedNumber.fromValue(kGTotalSupply, 0, "fixed32x18")
         .mulUnsafe(FixedNumber.fromString(KGUILDER_USDPRICE.toString(), "fixed32x18"))
-        .addUnsafe(FixedNumber.fromString("1", "fixed32x18")) // round up one wei
+        // .addUnsafe(FixedNumber.fromString("1", "fixed32x18")) // round up one wei
         .round(0)
         .toFormat("fixed32x0")
         .toString(),
+      18,
     );
+
+    logMessage(serviceName, `kCUR total supply: ${fromWei(kCurTotalSupply, 18)}`);
+    logMessage(serviceName, `kG total supply: ${fromWei(kGTotalSupply, 18)}`);
 
     logMessage(serviceName, `kCUR total value: ${kCurTotalValue.toString()}`);
     logMessage(serviceName, `kG total value: ${kGTotalValue.toString()}`);
 
-    if (kCurTotalValue.lt(kGTotalValue)) {
+    if (kCurTotalValue < kGTotalValue) {
       /**
        * then need to increase the balance of kCUR in the MentoReserve.
-       * round up cause we don't want a fractional number of tokens, and rounding down wouldn.t give us enough.
+       * round up cause we don't want a fractional number of tokens, and rounding down wouldn't give us enough.
        */
-      const amount = kGTotalValue.sub(kCurTotalValue);
+      const amountOfKCur = toWei((kGTotalValue - kCurTotalValue) / kCurPrice, 18).add(1);
       const relayerBalance = await kCurContract.balanceOf(relayerAddress);
 
-      if (relayerBalance.lt(amount)) {
+      if (relayerBalance.lt(amountOfKCur)) {
         throw new Error(
           `The Relayer has insufficient balance (${fromWei(
             relayerBalance,
             18,
-          )}) to send to the MentoReserve (needs: ${fromWei(amount, 18)})`,
+          )}) to send to the MentoReserve (needs: ${fromWei(amountOfKCur, 18)})`,
         );
       }
-      const tx: ITransaction = await kCurContract.transfer(mentoReserveContract.address, amount);
-      logMessage(serviceName, `Transferred ${fromWei(amount, 18)} kCur to the MentoReserve, tx hash: ${tx.hash}`);
-    } else if (kGTotalValue.lt(kCurTotalValue)) {
+
+      const tx: ITransaction = await kCurContract.transfer(mentoReserveContract.address, amountOfKCur);
+      logMessage(serviceName, `Transferred ${fromWei(amountOfKCur, 18)} kCur to the MentoReserve, tx hash: ${tx.hash}`);
+    } else if (kGTotalValue < kCurTotalValue) {
       /**
        * then need to decrease the balance of kCUR in the MentoReserve.
-       * round up cause we don't want a fractional number of tokens, and rounding down wouldn.t give us enough.
+       * round up cause we don't want a fractional number of tokens, and rounding down wouldn't give us enough.
        */
-      const amount = kCurTotalValue.sub(kGTotalValue);
+      const amountOfKCur = toWei((kCurTotalValue - kGTotalValue) / kCurPrice, 18).add(1);
       const mentoExchangeAvailable = await mentoReserveContract.getUnfrozenBalance();
 
-      if (mentoExchangeAvailable.lt(amount)) {
+      if (mentoExchangeAvailable.lt(amountOfKCur)) {
         throw new Error(
           `The MentoExchange has insufficient balance (${fromWei(
             mentoExchangeAvailable,
             18,
-          )}) to send to the Relayer (needs: ${fromWei(amount, 18)})`,
+          )}) to send to the Relayer (needs: ${fromWei(amountOfKCur, 18)})`,
         );
       }
 
-      const tx: ITransaction = await mentoReserveContract.transferExchangeGold(relayerAddress, amount);
-      logMessage(serviceName, `Transferred ${fromWei(amount, 18)} kCur from the MentoReserve, tx hash: ${tx.hash}`);
+      const tx: ITransaction = await mentoReserveContract.transferExchangeGold(relayerAddress, amountOfKCur);
+      // eslint-disable-next-line prettier/prettier
+      logMessage(serviceName, `Transferred ${fromWei(amountOfKCur, 18)} kCur from the MentoReserve, tx hash: ${tx.hash}`);
+    } else {
+      logMessage(serviceName, `No changes required, the numbers are balanced`);
     }
   } catch (ex) {
     serviceThrewException(serviceName, ex);
